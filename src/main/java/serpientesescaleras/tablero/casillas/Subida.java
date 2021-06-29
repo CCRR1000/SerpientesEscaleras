b48036d4291f938109bb62cf515f1487310a07d2
Michelle Cifuentes <j.micro.0101@gmail.com>
2021-06-29 13:15:48 -0600

    Se creó la casilla Subida

---
+++ src/main/java/serpientesescaleras/tablero/casillas/Subida.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serpientesescaleras.tablero.casillas;
+
+/**
+ *
+ * @author CIROSS
+ */
+public class Subida extends Casilla {
+    
+    private int filaFinal, columnaFinal;
+
+    public Subida(int fila, int columna, int filaFinal, int columnaFinal) {
+        super(fila, columna);
+        this.filaFinal = filaFinal;
+        this.columnaFinal = columnaFinal;
+    }
+
+    public int getFilaFinal() {
+        return filaFinal;
+    }
+
+    public void setFilaFinal(int filaFinal) {
+        this.filaFinal = filaFinal;
+    }
+
+    public int getColumnaFinal() {
+        return columnaFinal;
+    }
+
+    public void setColumnaFinal(int columnaFinal) {
+        this.columnaFinal = columnaFinal;
+    }
+    
+    
+}
