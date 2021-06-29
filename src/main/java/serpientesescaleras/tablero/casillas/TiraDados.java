15761288408ee0154ef394d80f284f591718ed4f
Michelle Cifuentes <j.micro.0101@gmail.com>
2021-06-29 13:16:19 -0600

    Se creó la casilla Tira dados

---
+++ src/main/java/serpientesescaleras/tablero/casillas/TiraDados.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serpientesescaleras.tablero.casillas;
+
+/**
+ *
+ * @author CIROSS
+ */
+public class TiraDados extends Casilla {
+
+    public TiraDados(int fila, int columna) {
+        super(fila, columna);
+    }
+    
+    
+}
