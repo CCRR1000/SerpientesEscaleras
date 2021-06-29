8befb81ca4c87597a5b95df9b2b7645453aa383a
Michelle Cifuentes <j.micro.0101@gmail.com>
2021-06-29 13:14:49 -0600

    Se creó la casilla Pierde turno

---
+++ src/main/java/serpientesescaleras/tablero/casillas/PierdeTurno.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serpientesescaleras.tablero.casillas;
+
+/**
+ *
+ * @author CIROSS
+ */
+public class PierdeTurno extends Casilla {
+
+    public PierdeTurno(int fila, int columna) {
+        super(fila, columna);
+    }
+    
+    
+}
