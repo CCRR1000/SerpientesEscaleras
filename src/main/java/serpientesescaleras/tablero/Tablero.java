05ed9e093506cc23c5d0dbb9595c605ea5dfb034
Michelle Cifuentes <j.micro.0101@gmail.com>
2021-06-29 00:43:14 -0600

    Creación de la clase Tablero

--- src/main/java/serpientesescaleras/tablero/Tablero.java
+++ src/main/java/serpientesescaleras/tablero/Tablero.java
@@ -5,10 +5,62 @@
  */
 package serpientesescaleras.tablero;
 
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 /**
  *
  * @author CIROSS
  */
 public class Tablero {
+
+    JPanel panel;
+    JLabel[][] matriz;
+    int filas, columnas, altoFila, anchoColumna;
+
+    public Tablero(JPanel jPanel) {
+        this.panel = jPanel;
+    }
+
+    public void crearMatriz(int filas, int columnas) {
+        panel.removeAll();
+
+        matriz = new JLabel[filas][columnas];
+        this.altoFila = panel.getHeight()/ filas;
+        this.anchoColumna = panel.getWidth()/ columnas;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                JLabel lbl = new JLabel("");
+                lbl.setBounds(this.anchoColumna * j, this.altoFila * i, this.anchoColumna, this.altoFila);
+                lbl.setHorizontalAlignment(SwingConstants.CENTER);
+                lbl.setForeground(Color.black);
+                lbl.setOpaque(true);
+                matriz[i][j] = lbl;
+                if ((i + j) % 2 == 0) {
+                    matriz[i][j].setBackground(new java.awt.Color(160, 82, 65));
+                } else {
+                    matriz[i][j].setBackground(new java.awt.Color(245, 222, 179));
+                }
+                if (j % 2 == 0) {
+                    matriz[i][j].setText("<");
+                } else {
+                    matriz[i][j].setText(">");
+                }
+            }
+        }
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                panel.add(matriz[i][j]);
+            }
+        }
+        
+        panel.repaint();
+    }
     
+    
+
 }
