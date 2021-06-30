50ccc453e93ab1111ced458591bc7ae0aa52c590
Michelle Cifuentes <j.micro.0101@gmail.com>
2021-06-30 16:10:37 -0600

    Se modificó el paquete fichas

--- src/main/java/serpientesescaleras/tablero/fichas/Ficha.java
+++ src/main/java/serpientesescaleras/tablero/fichas/Ficha.java
@@ -5,10 +5,73 @@
  */
 package serpientesescaleras.tablero.fichas;
 
+import javax.swing.Icon;
+import serpientesescaleras.tablero.casillas.Casilla;
+
+
 /**
  *
  * @author CIROSS
  */
 public class Ficha {
+
+    private int posicion, filaF, columnaF;
+    private boolean puedeMover;
+    private Icon icono;
+    private Casilla casillaActual;
+
+    public boolean isPuedeMover() {
+        return puedeMover;
+    }
+
+    public void setPuedeMover(boolean puedeMover) {
+        this.puedeMover = puedeMover;
+    }
+
+    public Icon getIcono() {
+        return icono;
+    }
+
+    public void setIcono(Icon icono) {
+        this.icono = icono;
+    }
+
+    public Ficha() {
+        this.puedeMover = true;
+    }
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+
+    public int getFilaF() {
+        return filaF;
+    }
+
+    public void setFilaF(int filaF) {
+        this.filaF = filaF;
+    }
+
+    public int getColumnaF() {
+        return columnaF;
+    }
+
+    public void setColumnaF(int columnaF) {
+        this.columnaF = columnaF;
+    }
+
+    public boolean isTurnoActivo() {
+        return puedeMover;
+    }
+
+    public void setTurnoActivo(boolean turnoActivo) {
+        this.puedeMover = turnoActivo;
+    }
+    
+    
     
 }
